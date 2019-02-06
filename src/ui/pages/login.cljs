f0357c5b3d7511405e51df6cf58c3c755308d81a
victorbjelkholm <victorbjelkholm@gmail.com>
2019-02-06 14:02:50 +0100

    Smaller login inputs
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/ui/pages/login.cljs
+++ src/ui/pages/login.cljs
@@ -24,7 +24,7 @@
     [:div.mv3
      (text-input {:id "username"
                   :label "Username"
-                  :class "w-20"
+                  :class "w6"
                   :style {:margin "0px auto"}
                   :type "text"
                   :value @username
@@ -32,14 +32,14 @@
     [:div.mv3
      (text-input {:id "password"
                   :label "Password"
-                  :class "w-20"
+                  :class "w6"
                   :style {:margin "0px auto"}
                   :type "password"
                   :value @password
                   :onChange #(reset! password (get-value %))})]
     (text-input {:id "login"
                  :style {:margin "0px auto"}
-                 :class "w-10 bg-aqua white b"
+                 :class "w4 bg-aqua white b"
                  :type "submit"
                  :value "Login"
                  :onChange #(do-login @username @password)})
